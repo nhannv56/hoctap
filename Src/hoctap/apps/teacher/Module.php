c4a75b207f2d67706e2f7ece1c78bbbd07d2f20e
Nhan <ngo.nhan.09@gmail.com>
2015-11-30 22:49:14 +0700

    setup view of formula math

--- Src/hoctap/apps/teacher/Module.php
+++ Src/hoctap/apps/teacher/Module.php
@@ -9,53 +9,53 @@ use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
 use Phalcon\Mvc\ModuleDefinitionInterface;
 use Phalcon\Config\Adapter\Ini;
 
-class Module implements ModuleDefinitionInterface
-{
-    /**
-     * Registers an autoloader related to the module
-     *
-     * @param DiInterface $di
-     */
-    public function registerAutoloaders(DiInterface $di = null)
-    {
+class Module implements ModuleDefinitionInterface {
+	/**
+	 * Registers an autoloader related to the module
+	 *
+	 * @param DiInterface $di        	
+	 */
+	public function registerAutoloaders(DiInterface $di = null) {
+		$loader = new Loader ();
 		
-        $loader = new Loader();
-
-        $loader->registerNamespaces(array(
-            'hoctap\Teacher\Controllers' => __DIR__ . '/controllers/',
-            'hoctap\Teacher\Models' => __DIR__ . '/models/',
-        ));
-
-        $loader->register();
-    }
-
-    /**
-     * Registers services related to the module
-     *
-     * @param DiInterface $di
-     */
-    public function registerServices(DiInterface $di)
-    {
-        /**
-         * Read configuration
-         */
-        $config = new Ini(APP_PATH  . "/apps/teacher/config/config.ini");
-
-        /**
-         * Setting up the view component
-         */
-        $di['view'] = function () {
-            $view = new View();
-            $view->setViewsDir(__DIR__ . '/views/');
-
-            return $view;
-        };
-
-        /**
-         * Database connection is created based in the parameters defined in the configuration file
-         */
-        $di['db'] = function () use ($config) {
-            return new DbAdapter($config->toArray());
-        };
-    }
+		$loader->registerNamespaces ( array (
+				'hoctap\Teacher\Controllers' => __DIR__ . '/controllers/',
+				'hoctap\Teacher\Models' => __DIR__ . '/models/',
+				'hoctap\Form\MathForm' => APP_PATH . '/forms' 
+		) );
+		
+		$loader->register ();
+	}
+	
+	/**
+	 * Registers services related to the module
+	 *
+	 * @param DiInterface $di        	
+	 */
+	public function registerServices(DiInterface $di) {
+		/**
+		 * Read configuration
+		 */
+		$config = new Ini ( APP_PATH . "/apps/teacher/config/config.ini" );
+		
+		/**
+		 * Setting up the view component
+		 */
+		$di ['view'] = function () {
+			$view = new View ();
+			$view->setViewsDir ( __DIR__ . '/views/' );
+			
+			// set layout
+			$view->setLayoutsDir ( '../../../layouts/' );
+			$view->setTemplateAfter ( 'main' );
+			return $view;
+		};
+		
+		/**
+		 * Database connection is created based in the parameters defined in the configuration file
+		 */
+		$di ['db'] = function () use($config) {
+			return new DbAdapter ( $config->toArray () );
+		};
+	}
 }
---
+++ Src/hoctap/apps/teacher/controllers/PostExamController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace hoctap\Teacher\Controllers;
+
+use hoctap\Teacher\Controllers\ControllerBase;
+use hoctap\Form\MathForm\MathQuestionForm;
+
+class PostExamController extends ControllerBase {
+	public function initialize() {
+		parent::initialize ();
+		$this->tag->prependTitle ( 'Tạo đề thi| ' );
+	}
+	public function indexAction() {
+	}
+	public function createAction() {
+		$question = new MathQuestionForm ();
+		$this->view->question = $question;
+	}
+}
+
