10cdc62640b00855d5db1298058742324b873bb3
Nhan <ngo.nhan.09@gmail.com>
2015-12-20 16:39:44 +0700

    add working status sample design for teacher module

--- Src/hoctap/forms/MathQuestionForm.php
+++ Src/hoctap/forms/MathQuestionForm.php
@@ -5,10 +5,11 @@ namespace hoctap\Form\MathForm;
 use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Element\Submit;
+use Phalcon\Forms\Element\TextArea;
 
 class MathQuestionForm extends Form {
 	public function initialize(){
-		$this->add(new Text("name"));
+		$this->add(new TextArea("name"));
 		$this->add(new Submit("OK"));
 	}
 }
