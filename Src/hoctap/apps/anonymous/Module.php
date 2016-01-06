389c68eafd04ea94ca0a45d3a6d87496b71d834c
Nhan <ngo.nhan.09@gmail.com>
2016-01-06 21:34:45 +0700

    sample for math online view and dynamic create question

--- Src/hoctap/apps/anonymous/Module.php
+++ Src/hoctap/apps/anonymous/Module.php
@@ -9,53 +9,55 @@ use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
 use Phalcon\Mvc\ModuleDefinitionInterface;
 use Phalcon\Config\Adapter\Ini;
 
-class Module implements ModuleDefinitionInterface
-{
-    /**
-     * Registers an autoloader related to the module
-     *
-     * @param DiInterface $di
-     */
-    public function registerAutoloaders(DiInterface $di = null)
-    {
+class Module implements ModuleDefinitionInterface {
+	/**
+	 * Registers an autoloader related to the module
+	 *
+	 * @param DiInterface $di        	
+	 */
+	public function registerAutoloaders(DiInterface $di = null) {
+		$loader = new Loader ();
 		
-        $loader = new Loader();
-
-        $loader->registerNamespaces(array(
-            'hoctap\Anonymous\Controllers' => __DIR__ . '/controllers/',
-            'hoctap\Anonymous\Models' => __DIR__ . '/models/',
-        ));
-
-        $loader->register();
-    }
-
-    /**
-     * Registers services related to the module
-     *
-     * @param DiInterface $di
-     */
-    public function registerServices(DiInterface $di)
-    {
-        /**
-         * Read configuration
-         */
-        $config = new Ini(APP_PATH  . "/apps/anonymous/config/config.ini");
-
-        /**
-         * Setting up the view component
-         */
-        $di['view'] = function () {
-            $view = new View();
-            $view->setViewsDir(__DIR__ . '/views/');
-
-            return $view;
-        };
-
-        /**
-         * Database connection is created based in the parameters defined in the configuration file
-         */
-        $di['db'] = function () use ($config) {
-            return new DbAdapter($config->toArray());
-        };
-    }
+		$loader->registerNamespaces ( array (
+				'hoctap\Anonymous\Controllers' => __DIR__ . '/controllers/',
+				'hoctap\Anonymous\Models' => __DIR__ . '/models/' 
+		) );
+		
+		$loader->register ();
+	}
+	
+	/**
+	 * Registers services related to the module
+	 *
+	 * @param DiInterface $di        	
+	 */
+	public function registerServices(DiInterface $di) {
+		/**
+		 * Read configuration
+		 */
+		$config = new Ini ( APP_PATH . "/apps/anonymous/config/config.ini" );
+		
+		/**
+		 * Setting up the view component
+		 */
+		$di ['view'] = function () {
+			$view = new View ();
+			$view->setViewsDir ( __DIR__ . '/views/' );
+			
+			return $view;
+		};
+		
+		/**
+		 * Database connection is created based in the parameters defined in the configuration file
+		 */
+		$di ['db'] = function () use($config) {
+			// return new DbAdapter($config->toArray());
+			return new DbAdapter ( array (
+					'host' => $config->database->host,
+					'username' => $config->database->username,
+					'password' => $config->database->password,
+					'dbname' => $config->database->dbname 
+			) );
+		};
+	}
 }
--- Src/hoctap/apps/anonymous/controllers/UserController.php
+++ Src/hoctap/apps/anonymous/controllers/UserController.php
@@ -1,21 +1,46 @@
 <?php
+
 namespace hoctap\Anonymous\Controllers;
 
 use hoctap\Anonymous\Controllers\ControllerBase;
-class UserController extends ControllerBase
-{
-	public function initialize(){
-		parent::initialize();
+use hoctap\Anonymous\Models\Users;
+
+class UserController extends ControllerBase {
+	public function initialize() {
+		parent::initialize ();
 	}
-    public function indexAction()
-    {
-		
-    }
-    public function registerAction() {
-    	
-    }
-    public function loginAction(){
-    	
-    }
+	public function indexAction() {
+	}
+	public function registerAction() {
+		if ($this->request->isPost ()) {
+			$newUser = new Users ();
+			$newUser->user_name = $this->request->getPost ( 'user_name' );
+			$newUser->first_name = $this->request->getPost ( 'first_name' );
+			$newUser->email = $this->request->getPost ( 'email' );
+			$passwordText = $this->request->getPost ( 'password' );
+			$newUser->id = 1;
+			$password = md5 ( $passwordText, false );
+			$newUser->password = $password;
+			if (! $newUser->save ()) {
+				foreach ( $newUser->getMessages () as $message ) {
+					$this->flash->error ( $message );
+				}
+				return $this->dispatcher->forward ( array (
+						'module' => 'anonymous',
+						"controller" => "user",
+						"action" => "register" 
+				) );
+			}
+			$this->flash->success ( "user was created successfully" );
+			
+			return $this->dispatcher->forward ( array (
+					"module" => 'student',
+					"controller" => "index",
+					"action" => "index" 
+			) );
+		}
+	}
+	public function loginAction() {
+	}
 }
 
