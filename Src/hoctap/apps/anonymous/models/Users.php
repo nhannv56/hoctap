be1175c57fe35853878cb980fc8518e05be8d9f7
Nhan <ngo.nhan.09@gmail.com>
2016-01-06 21:35:21 +0700

    user model create for register/login ...

---
+++ Src/hoctap/apps/anonymous/models/Users.php
@@ -0,0 +1,149 @@
+<?php
+namespace hoctap\Anonymous\Models;
+use Phalcon\Mvc\Model\Validator\Email as Email;
+
+class Users extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $first_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $last_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $mobile_phone;
+
+    /**
+     *
+     * @var string
+     */
+    public $bithday;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     *
+     * @var string
+     */
+    public $facebook;
+
+    /**
+     *
+     * @var string
+     */
+    public $address;
+
+    /**
+     *
+     * @var string
+     */
+    public $school;
+
+    /**
+     *
+     * @var integer
+     */
+    public $education_level;
+
+    /**
+     *
+     * @var integer
+     */
+    public $rank;
+
+    /**
+     *
+     * @var string
+     */
+    public $user_name;
+
+    /**
+     *
+     * @var string
+     */
+    public $password;
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation()
+    {
+        $this->validate(
+            new Email(
+                array(
+                    'field'    => 'email',
+                    'required' => true,
+                )
+            )
+        );
+
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->hasMany('id', 'Questions', 'author_id', array('alias' => 'Questions'));
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'users';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
